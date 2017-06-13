5e53c32ca8c651eb005ef3867cfc3e315187cc5b
Vasko <vasko5petrov@gmail.com>
2017-06-13 17:47:56 +0300

    ngx-pagination

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,7 +16,7 @@ import { Ng2Bs3ModalModule } from 'ng2-bs3-modal/ng2-bs3-modal';
 
 import { Ng2OrderModule } from 'ng2-order-pipe';
 
-import {Ng2PaginationModule} from 'ng2-pagination';
+import {NgxPaginationModule} from 'ngx-pagination';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
@@ -120,7 +120,7 @@ export class OrderBy{
     Daterangepicker,
     Ng2Bs3ModalModule,
     Ng2OrderModule,
-    Ng2PaginationModule
+    NgxPaginationModule
   ],
   providers: [FirebaseService],
   bootstrap: [AppComponent]
